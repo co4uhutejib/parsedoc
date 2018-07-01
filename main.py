399f27829be0936870dfa1993617bd9e21ee6855
co4uhutejib <mister-fokin@yandex.ru>
2018-07-01 18:04:21 +0300

    hardcode first cell

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ from parse_table import prepare_data
 
 parser = argparse.ArgumentParser(description='process doc table.')
 parser.add_argument('doc', help='input file')
-parser.add_argument('first_cell', help='first cell in table')
+# parser.add_argument('first_cell', help='first cell in table')
 
 args = parser.parse_args()
 
-data = prepare_data(args.doc, args.first_cell)
+data = prepare_data(args.doc, u"Вид профессиональной деятельности")
 assert data != None
+
+
--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
-python3 main.py in_doc.docx "Вид профессиональной деятельности"
+python3 main.py in_doc.docx
+
+
+#python3 main.py in_doc.docx "Вид профессиональной деятельности"
