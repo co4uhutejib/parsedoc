5adc6959019f35e2e0adf3f1b16d022d1701aafd
co4uhutejib <mister-fokin@yandex.ru>
2018-07-08 22:35:53 +0300

    little upd

--- main.py
+++ main.py
@@ -6,14 +6,41 @@ import argparse
 
 from parse_table import prepare_data
 
-parser = argparse.ArgumentParser(description='process doc table.')
+parser = argparse.ArgumentParser(description='Process doc table.')
 parser.add_argument('doc', help='input file')
 # parser.add_argument('first_cell', help='first cell in table')
 
+subparsers = parser.add_subparsers(help='sub-command help')
+
+#
+parser_competence = subparsers.add_parser('competence',
+    help='Print competence description: what should know, able and master.')
+parser_competence.add_argument('NAME',
+    help='Name of competence.')
+parser_competence.set_defaults(which='competence')
+
+#
+parser_competence = subparsers.add_parser('discipline',
+    help='FInd discipline if it exist.')
+parser_competence.add_argument('NAME',
+    help='Discipline name.')
+parser_competence.set_defaults(which='discipline')
+
+# TODO 
+# parser_competence = subparsers.add_parser('methods',
+#     help='Print all from "Methods and technologies formation competence". Duplicate will be removed.')
+
+
 args = parser.parse_args()
 
-data = prepare_data(args.doc, u"Вид профессиональной деятельности")
-assert data != None
+# data = prepare_data(args.doc, u"Вид профессиональной деятельности")
+# assert data != None
 
+print(args)
+if args.which == 'competence':
+    # TODO
+    print('print competence competence')
 
-# TODO
+if args.which == 'discipline':
+    # TODO
+    print('find discipline')
--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-python3 main.py in_doc.docx
+python3 main.py in_doc.docx competence "ОК-6"
+
+# python3 main.py in_doc.docx 
 
 
 #python3 main.py in_doc.docx "Вид профессиональной деятельности"
